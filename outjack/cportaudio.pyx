9f74b787fdcaa645cd4c4b4dad4b6fd9dd14dca5
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-22 18:56:47 +0000

    uhuh

--- outjack/cportaudio.pyx
+++ outjack/cportaudio.pyx
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with outjack.  If not, see <http://www.gnu.org/licenses/>.
 
+# cython: language_level=3
+
 cdef extern from "portaudio.h":
 
     ctypedef int PaError
