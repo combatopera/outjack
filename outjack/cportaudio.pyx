00c5782e48463824b458436c4c3fb29124657aa4
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-12-22 17:01:16 +0000

    wip

---
+++ outjack/cportaudio.pyx
@@ -0,0 +1,31 @@
+# Copyright 2017, 2020 Andrzej Cichocki
+
+# This file is part of outjack.
+#
+# outjack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# outjack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with outjack.  If not, see <http://www.gnu.org/licenses/>.
+
+cdef extern from "portaudio.h":
+
+    ctypedef int PaError
+
+    PaError Pa_Initialize()
+    PaError Pa_Terminate()
+
+cdef class Client:
+
+    def __init__(self):
+        Pa_Initialize()
+
+    def dispose(self):
+        Pa_Terminate()
