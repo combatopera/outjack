9b0d00a2b373408c2f54a28cafbb2d07e626cc76
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-11-30 23:47:16 +0000

    remove pyximport for now

--- outjack/__init__.py
+++ outjack/__init__.py
@@ -15,6 +15,3 @@
 # You should have received a copy of the GNU General Public License
 # along with outjack.  If not, see <http://www.gnu.org/licenses/>.
 
-from diapyr.util import initnative
-
-initnative()
